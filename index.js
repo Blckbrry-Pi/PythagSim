0d7d1957d48594fcc8b2f54555d19b06302d4371
Blckbrry-Pi <54462713+Blckbrry-Pi@users.noreply.github.com>
2020-04-01 10:51:19 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
+let sliderPercentage;
 function setup() {
     textAlign(CENTER, CENTER);
+    
+    sliderPercentage = createSlider(10, 100, 50, 1);
+    sliderPercentage.position(10, 10);
+    sliderPercentage.style('width', '80px');
 
 }
-
-let sliderPercentage
 function draw() {
     createCanvas(innerWidth, innerHeight);
     fill(255);
     
     background(255);
-    
-    slider = createSlider(10, 100, 50, 1);
-    slider.position(10, 10);
-    slider.style('width', '80px');
 }
