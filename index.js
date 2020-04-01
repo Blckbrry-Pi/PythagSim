f4423c7245d075d7122b92cbffd19f325e5e0454
Blckbrry-Pi <54462713+Blckbrry-Pi@users.noreply.github.com>
2020-04-01 11:31:51 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+let sliderSize;
 let sliderPercentage;
 function setup() {
     createCanvas(innerWidth, innerHeight);
     
     textAlign(CENTER, CENTER);
     
+    sliderSize = createSlider(50, 500, 50, 0.1);
     sliderPercentage = createSlider(10, 100, 50, 1);
 }
 function draw() {
@@ -11,7 +13,7 @@ function draw() {
     background(255);
     
     rect(0, height/10*9,  width, height/10);
-    text
+    text("Square Sizes", width/4, height/30*28);
     sliderPercentage.position(width/30, height - height/30);
     sliderPercentage.style('width', width/15*7);
 }
